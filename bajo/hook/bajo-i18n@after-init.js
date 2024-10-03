0121e8825cef7c65344a7f55052b7a5e43f25b43
Ardhi Lukianto <ardhi@lukianto.com>
2024-10-03 21:20:25 +0700

    Delete bajo-i18n@after-init.js

--- bajo/hook/bajo-i18n@after-init.js
+++
@@ -1,6 +0,0 @@
-async function bajoI18NAfterInit () {
-  const config = this.app.bajoI18N.config
-  if (!config.fallbackNS.includes(this.name)) config.fallbackNS.unshift(this.name)
-}
-
-export default bajoI18NAfterInit
